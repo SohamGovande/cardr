168aaa7dbd08c57e5f235203a9fad489bafc2bcd
Soham Govande <vgsoham@gmail.com>
2020-01-01 02:46:14 -0600

    Fixed the default card formatting

--- CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/prefs/PrefsObject.kt
+++ CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/prefs/PrefsObject.kt
@@ -15,6 +15,6 @@ class PrefsObject {
     var accessToken = ""
 
     companion object {
-        const val DEFAULT_CARD_FORMAT = "<html dir=\"ltr\"><head></head><body ><h4><font face=\"Calibri\" size=\"4\">{Tag}</font></h4><p><font face=\"Calibri\"><b><font size=\"4\">{AuthorShort}, {DateShort}</font></b> ({Author}, {Qualifications}{Date}, accessed on {CurrentDate}, {Publication}, \"{Title}\", {Url})</font></p><p><font face=\"Calibri\">{CardBody}</font></p></body></html>\"{Title}\", {Url})"
+        const val DEFAULT_CARD_FORMAT = "<html dir=\"ltr\"><head></head><body ><h4><font face=\"Calibri\" size=\"4\">{Tag}</font></h4><p><font face=\"Calibri\"><b><font size=\"4\">{AuthorLastName},&nbsp;</font></b><font size=\"4\"><b>{DateShortened}</b></font>&nbsp;</font><span style=\"font-family: Calibri;\">({AuthorFullName}, {Qualifications}{DateFull}, accessed on {CurrentDate}, {Publication}, \"{Title}\", {Url})</span></p><p><font face=\"Calibri\">{CardBody}</font></p></body></html>"
     }
 }
