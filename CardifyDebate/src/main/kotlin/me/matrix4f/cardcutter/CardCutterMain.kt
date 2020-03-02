9ae636aba84eb456dbd8121828051eebbee4affa
Soham Govande <vgsoham@gmail.com>
2020-03-02 09:17:43 -0600

    Added java.net.preferIPv4Stack=true property

--- CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/CardCutterMain.kt
+++ CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/CardCutterMain.kt
@@ -54,6 +54,8 @@ fun main(args: Array<String>) {
     }
 
     setLoggerDir()
+    System.setProperty("java.net.preferIPv4Stack", "true")
+
     CardifyDebate.logger.info("Launching Cardify with the following arguments: ${Arrays.toString(args)}")
 
     checkForUpdates()
