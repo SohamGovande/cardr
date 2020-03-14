ac5b2adc2e80f1de8af48d9d0b398f3a0f3c301a
Soham Govande <vgsoham@gmail.com>
2020-03-14 16:12:33 -0500

    Added clarification message to edit card format window

--- CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/ui/windows/FormatPrefsWindow.kt
+++ CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/ui/windows/FormatPrefsWindow.kt
@@ -42,6 +42,9 @@ class FormatPrefsWindow: ModalWindow("Settings - Card Format") {
         val header = Label("Card Format")
         header.font = Font.font(20.0)
 
+        val subheader = Label("NOTE: The 12 pt font translates to 11 pt in Word/Google Docs, and 14 pt translates to 13 pt. On macOS, Helvetica translates to Calibri in Word/Google Docs. All other font families and sizes work as expected.")
+        subheader.isWrapText = true
+
         val resetBtn = Button("Reset to Default")
         val infoBtn = Button("Macro List")
         val btnHbox = HBox()
@@ -51,9 +54,12 @@ class FormatPrefsWindow: ModalWindow("Settings - Card Format") {
 
         editText.htmlText = Prefs.get().cardFormat
         editText.prefWidth = 600.0
-        editText.prefHeight = 400.0
+        editText.maxHeight = 225.0
         editText.padding = Insets(1.0)
-        editText.border = Border(BorderStroke(Color.web("#ddd"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))
+        if (Prefs.get().darkMode)
+            editText.border = Border(BorderStroke(Color.web("#222"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))
+        else
+            editText.border = Border(BorderStroke(Color.web("#ddd"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT))
         editText.style = "-fx-font-size: 14.0; -fx-font-family: 'Calibri';"
 
         val editHBox = HBox()
@@ -109,6 +115,7 @@ class FormatPrefsWindow: ModalWindow("Settings - Card Format") {
         }
 
         vbox.children.add(header)
+        vbox.children.add(subheader)
         vbox.children.add(editHBox)
         vbox.children.add(btnHbox)
         vbox.children.add(applyBtn)
