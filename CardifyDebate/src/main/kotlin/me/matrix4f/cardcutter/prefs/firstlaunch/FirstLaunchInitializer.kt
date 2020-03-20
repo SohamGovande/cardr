003e03b238db010435aebd6e7d32bda4ee5926d6
Soham Govande <vgsoham@gmail.com>
2020-03-19 21:29:38 -0500

    Fixed macOS launch error FileAlreadyExistsExceptino

--- CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/prefs/firstlaunch/FirstLaunchInitializer.kt
+++ CardifyDebate/src/main/kotlin/me/matrix4f/cardcutter/prefs/firstlaunch/FirstLaunchInitializer.kt
@@ -1,8 +1,7 @@
 package me.matrix4f.cardcutter.prefs.firstlaunch
 import me.matrix4f.cardcutter.prefs.Prefs
 import me.matrix4f.cardcutter.prefs.PrefsObject
-import me.matrix4f.cardcutter.util.OS
-import me.matrix4f.cardcutter.util.getOSType
+import me.matrix4f.cardcutter.util.*
 import net.lingala.zip4j.ZipFile
 import org.apache.commons.exec.CommandLine
 import org.apache.commons.exec.DefaultExecutor
@@ -12,8 +11,8 @@ import org.apache.logging.log4j.LogManager
 import java.io.*
 import java.net.URL
 import java.nio.channels.Channels
+import java.nio.file.FileAlreadyExistsException
 import java.nio.file.Files
-import java.nio.file.Path
 import java.nio.file.Paths
 
 private val logger = LogManager.getLogger(Prefs::class.java)
@@ -62,7 +61,7 @@ private fun downloadChromeDataMacOS() {
 
     val executablePath = Paths.get(System.getProperty("user.home"), "CardifyDebate", "CardifyChromeApp")
     val executableZipPath = Paths.get(System.getProperty("user.home"), "CardifyDebate", "CardifyChromeApp.zip")
-    Files.createDirectories(executablePath.parent)
+    try { Files.createDirectories(executablePath.parent) } catch (e: FileAlreadyExistsException) { }
 
     logger.info("Opening data stream for json file...")
     var dataStream = URL("http://cardifydebate.x10.bz/data/mac/ChromeAppNativeJson-v1.2.0.json").openStream()
@@ -75,33 +74,14 @@ private fun downloadChromeDataMacOS() {
     data = data.replace("%FILEPATH%", executablePath.toFile().absolutePath)
     Files.write(jsonPath, data.toByteArray())
 
-    logger.info("Opening data stream for executable file...")
-    dataStream = URL("http://cardifydebate.x10.bz/data/mac/CardifyChromeApp-v1.2.0.zip").openStream()
-    logger.info("Downloading executable file...")
-    var fos = FileOutputStream(executableZipPath.toFile())
-    fos.channel.transferFrom(Channels.newChannel(dataStream), 0, Long.MAX_VALUE)
-    dataStream.close()
-    fos.close()
+    downloadFileFromURL("http://cardifydebate.x10.bz/data/mac/CardifyChromeApp-v1.2.0.zip", executableZipPath.toFile(), logger)
 
     logger.info("Extracting zipped executable...")
     val zipFile = ZipFile(executableZipPath.toFile())
     zipFile.extractAll(executablePath.parent.toFile().absolutePath)
     executableZipPath.toFile().deleteOnExit()
 
-    val cmd = "chmod +x \"${executablePath.toFile().absolutePath}\""
-    logger.info("Running command '$cmd'")
-    val stdout = ByteArrayOutputStream()
-    val stdoutPsh = PumpStreamHandler(stdout)
-    val cmdLine = CommandLine.parse(cmd)
-    val executor = DefaultExecutor()
-    executor.streamHandler = stdoutPsh
-    try {
-        executor.execute(cmdLine)
-    } catch (e: Exception) {
-    }
-
-    val result = stdout.toString().replace("\n", " ")
-    logger.info("Command '$cmd' returned '$result'")
+    makeFileExecutableViaChmod(executablePath.toFile().absolutePath, logger)
 
     if (!jsonPath.toFile().exists())
         throw FirstLaunchException("Unable to download Chrome App Native JSON.")
@@ -119,45 +99,22 @@ private fun onFirstLaunchWindows() {
         "REG ADD \"HKCU\\Software\\Google\\Chrome\\NativeMessagingHosts\\me.matrix4f.cardify\" /ve /t REG_SZ /d \"${jsonFile.absolutePath}\" /f"
     )
     for (cmd in commands) {
-        val stdout = ByteArrayOutputStream()
-
-        val stdoutPsh = PumpStreamHandler(stdout)
-        val cmdLine = CommandLine.parse(cmd)
-        val executor = DefaultExecutor()
-        executor.streamHandler = stdoutPsh
-        try {
-            executor.execute(cmdLine)
-        } catch (e: Exception) {
-        }
-
-        val result = stdout.toString().replace("\n", "")
-        logger.info("Command '$cmd' returned '$result'")
+        executeCommandBlocking(cmd, logger)
     }
 }
 
 @Throws(FirstLaunchException::class, Exception::class)
 private fun onFirstLaunchMacOS() {
     downloadChromeDataMacOS()
+
     val macScriptsPath = Paths.get(System.getProperty("user.home"), "CardifyDebate", "MacScripts")
-    Files.createDirectory(macScriptsPath)
+    try { Files.createDirectory(macScriptsPath) } catch (e: FileAlreadyExistsException) { }
+
     val getWordWindowsScriptPath = Paths.get(macScriptsPath.toFile().absolutePath, "getWordWindows.scpt")
     val selectWordWindowScriptPath = Paths.get(macScriptsPath.toFile().absolutePath, "selectWordWindow.scpt")
 
-    logger.info("Opening data stream for AppleScript 'getWordWindows'...")
-    var dataStream = URL("http://cardifydebate.x10.bz/data/mac/MacScripts/getWordWindows.scpt").openStream()
-    logger.info("Downloading AppleScript file for 'getWordWindows'...")
-    var fos = FileOutputStream(getWordWindowsScriptPath.toFile())
-    fos.channel.transferFrom(Channels.newChannel(dataStream), 0, Long.MAX_VALUE)
-    dataStream.close()
-    fos.close()
-
-    logger.info("Opening data stream for AppleScript 'selectWordWindow'...")
-    dataStream = URL("http://cardifydebate.x10.bz/data/mac/MacScripts/selectWordWindow.scpt").openStream()
-    logger.info("Downloading AppleScript file for 'selectWordWindow'...")
-    fos = FileOutputStream(selectWordWindowScriptPath.toFile())
-    fos.channel.transferFrom(Channels.newChannel(dataStream), 0, Long.MAX_VALUE)
-    dataStream.close()
-    fos.close()
+    downloadFileFromURL("http://cardifydebate.x10.bz/data/mac/MacScripts/getWordWindows.scpt", getWordWindowsScriptPath.toFile(), logger)
+    downloadFileFromURL("http://cardifydebate.x10.bz/data/mac/MacScripts/selectWordWindow.scpt", selectWordWindowScriptPath.toFile(), logger)
 
     if (!selectWordWindowScriptPath.toFile().exists())
         throw FirstLaunchException("Unable to download AppleScript 'selectWordWindow'.")
