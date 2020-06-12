c7bfbf4eb15315e6441c669b7cbaa11c9b57b0a2
Soham Govande <vgsoham@gmail.com>
2020-06-12 17:32:17 -0500

    Fixed bug: OCR window dimensions don't save on mac

--- CardrDesktop/src/main/kotlin/me/sohamgovande/cardr/core/ui/WindowDimensions.kt
+++ CardrDesktop/src/main/kotlin/me/sohamgovande/cardr/core/ui/WindowDimensions.kt
@@ -2,13 +2,14 @@ package me.sohamgovande.cardr.core.ui
 
 import javafx.stage.Screen
 import javafx.stage.Stage
+import javafx.stage.StageStyle
 
 data class WindowDimensions(var x: Double, var y: Double, var w: Double, var h: Double, var maximized: Boolean) {
 
     constructor(stage: Stage) : this(stage.x, stage.y, stage.width, stage.height, stage.isMaximized)
 
     fun apply(stage: Stage) {
-        if (!maximized) {
+        if (!maximized || stage.style == StageStyle.TRANSPARENT) {
             stage.x = x
             stage.y = y
             stage.width = w
